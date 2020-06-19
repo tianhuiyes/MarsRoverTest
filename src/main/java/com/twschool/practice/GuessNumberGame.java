64f312a4d2142a2aa8aedc81befeef5d31741685
huihui <huihui@example.com>
2020-06-19 18:31:25 +0800

    should_return_SUCCEED_given_gameAnswer_1234_and_userAnswer_1234

---
+++ src/main/java/com/twschool/practice/GuessNumberGame.java
@@ -0,0 +1,13 @@
+package com.twschool.practice;
+
+import static com.twschool.practice.GameStatus.SUCCEED;
+
+public class GuessNumberGame {
+    public GuessNumberGame(Answer answer) {
+
+    }
+
+    public GameStatus guess(String userAnswer) {
+        return SUCCEED;
+    }
+}
--- src/test/java/com/twschool/practice/GuessNumberGameTest.java
+++ src/test/java/com/twschool/practice/GuessNumberGameTest.java
@@ -6,8 +6,15 @@ import org.junit.Test;
 public class GuessNumberGameTest {
 
     @Test
-    public void should_return_SUCCEED_() {
+    public void should_return_SUCCEED_given_gameAnswer_1234_and_userAnswer_1234() {
+        //given
+        Answer answer = new Answer("1 2 3 4 ");
+        GuessNumberGame guessNumberGame = new GuessNumberGame(answer);
 
-        Assert.assertTrue(true);
+        //when
+        GameStatus gameStatus =guessNumberGame.guess("1 2 3 4");
+
+        //then
+        Assert.assertEquals(GameStatus.SUCCEED,gameStatus);
     }
 }
