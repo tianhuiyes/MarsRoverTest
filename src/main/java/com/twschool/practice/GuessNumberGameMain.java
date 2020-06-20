9f7b14f27032d4fafd0525785023626fafbee8a4
huihui <huihui@example.com>
2020-06-20 14:59:48 +0800

    run jar use cmd

--- src/main/java/com/twschool/practice/GuessNumberGameMain.java
+++ src/main/java/com/twschool/practice/GuessNumberGameMain.java
@@ -4,15 +4,12 @@ import java.util.Scanner;
 
 public class GuessNumberGameMain {
     public static void main(String[] args) {
-        //启动游戏
         Scanner scan = new Scanner(System.in);
         System.out.println("Welcome, Let's guess number!");
 
-        //生成游戏答案
         Generator generator = new Generator();
         String gameAnswer = generator.getAnswer();
 
-        //用户输入，校验输入格式，对照游戏答案，输出相应的游戏状态
         Answer answer = new Answer(gameAnswer);
         GuessNumberGame guessNumberGame = new GuessNumberGame(answer);
 
